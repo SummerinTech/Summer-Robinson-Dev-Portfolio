9a8dc2088b135a626e9ea4eeca1c0a6c74ead8d2
Summer <72228259+SummerinTech@users.noreply.github.com>
2021-12-08 17:50:32 -0800

    remove drumkit app and replace with dictionary app

--- src/script.js
+++ src/script.js
@@ -1,1 +1,9 @@
+const hamburger = document.querySelector(".hamburger");
+const mobileMenu = document.querySelector(".mobile-nav-bar");
 
+function showMobileMenu() {
+	mobileMenu.classList.remove = "hidden";
+	mobileMenu.classList.add = "show";
+}
+
+hamburger.addEventListener("click", showMobileMenu);
